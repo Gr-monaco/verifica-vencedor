6b4c7a60785b0a912b3f48da9839e31d230614a4
Gr-monaco <gr-monaco@hotmail.com>
2022-09-26 20:19:55 -0300

    Termina aplicação de console após execução

--- src/main/java/gr/monaco/verificavencedor/VerificavencedorApplication.java
+++ src/main/java/gr/monaco/verificavencedor/VerificavencedorApplication.java
@@ -4,13 +4,14 @@ import gr.monaco.verificavencedor.models.Deck;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class VerificavencedorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VerificavencedorApplication.class, args);
+		SpringApplication.run(VerificavencedorApplication.class, args).close();
 	}
 
 	@Override
