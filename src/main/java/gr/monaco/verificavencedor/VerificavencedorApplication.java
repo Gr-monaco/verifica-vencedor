e10b7d8913302b3fb6bd63942861f4edfbe8bc92
Gr-monaco <gr-monaco@hotmail.com>
2022-09-26 19:20:00 -0300

    Commit inicial

---
+++ src/main/java/gr/monaco/verificavencedor/VerificavencedorApplication.java
@@ -0,0 +1,13 @@
+package gr.monaco.verificavencedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificavencedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificavencedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/monaco/verificavencedor/VerificavencedorApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.monaco.verificavencedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificavencedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
