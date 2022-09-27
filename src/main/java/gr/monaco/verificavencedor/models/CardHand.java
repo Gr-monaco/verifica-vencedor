5f2a60ab7f6fc3d048cd6129c8ca6785828fe4c0
Gr-monaco <gr-monaco@hotmail.com>
2022-09-26 22:01:29 -0300

    Implementar Serializable em CardHand.java

--- src/main/java/gr/monaco/verificavencedor/models/CardHand.java
+++ src/main/java/gr/monaco/verificavencedor/models/CardHand.java
@@ -3,8 +3,10 @@ package gr.monaco.verificavencedor.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class CardHand {
+public class CardHand implements Serializable {
     private String sucess;
     @JsonProperty("deck_id")
     private String Id;
