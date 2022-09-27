908a1e63071cf09fd1e78f20311d999f09f902ab
Gr-monaco <gr-monaco@hotmail.com>
2022-09-26 21:56:48 -0300

    Implementar modelo de Images

---
+++ src/main/java/gr/monaco/verificavencedor/models/Images.java
@@ -0,0 +1,11 @@
+package gr.monaco.verificavencedor.models;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class Images implements Serializable {
+    private String svg;
+    private String png;
+}
