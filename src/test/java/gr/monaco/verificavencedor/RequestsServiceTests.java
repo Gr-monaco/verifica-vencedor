75a3d50562482d69e5cea4098faab6230498bca0
Gr-monaco <gr-monaco@hotmail.com>
2022-09-26 21:28:18 -0300

    Tornar deck global em RequestsServiceTests

--- src/test/java/gr/monaco/verificavencedor/RequestsServiceTests.java
+++ src/test/java/gr/monaco/verificavencedor/RequestsServiceTests.java
@@ -2,21 +2,37 @@ package gr.monaco.verificavencedor;
 
 import gr.monaco.verificavencedor.models.Deck;
 import gr.monaco.verificavencedor.services.RequestsService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Objects;
+
 @SpringBootTest
+@Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RequestsServiceTests {
     @Autowired
     private RequestsService requestsService;
 
+    private ResponseEntity<Deck> deck;
+
+    @BeforeAll
     @Test
     void verificaSeRequesicaoRetornaDeckStatusCode(){
-        ResponseEntity<Deck> deck = requestsService.getDeck();
+        deck = requestsService.getDeck();
+        log.info("DECK-ID: {}", Objects.requireNonNull(deck.getBody()).getId());
         Assertions.assertEquals(deck.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    void verificaSeDeckFoiEmbaralhado(){
+        Assertions.assertTrue(Objects.requireNonNull(deck.getBody()).isShuffled());
     }
 }
