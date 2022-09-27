cd2d6ec6f1ad6e6a02a7300f61eab6642cd2aca2
Gr-monaco <gr-monaco@hotmail.com>
2022-09-26 22:04:17 -0300

    Adicionar funcao que busca mao

--- src/main/java/gr/monaco/verificavencedor/services/RequestsService.java
+++ src/main/java/gr/monaco/verificavencedor/services/RequestsService.java
@@ -1,5 +1,6 @@
 package gr.monaco.verificavencedor.services;
 
+import gr.monaco.verificavencedor.models.CardHand;
 import gr.monaco.verificavencedor.models.Deck;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,9 @@ public class RequestsService {
 
     public ResponseEntity<Deck> getDeck(){
         return new RestTemplate().getForEntity(APIURL+"new/shuffle/?deck_count=1", Deck.class);
+    }
+
+    public ResponseEntity<CardHand> getHand(String deckId, int amountOfCards){
+        return  new RestTemplate().getForEntity(APIURL+deckId+"/draw/?count="+ amountOfCards, CardHand.class);
     }
 }
