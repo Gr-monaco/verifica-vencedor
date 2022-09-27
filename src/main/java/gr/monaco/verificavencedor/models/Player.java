9e17a710eab6b0f14aa91c82907e0326baf2fbc4
Gr-monaco <gr-monaco@hotmail.com>
2022-09-27 10:22:59 -0300

    Adicionar classe de Jogador

---
+++ src/main/java/gr/monaco/verificavencedor/models/Player.java
@@ -0,0 +1,10 @@
+package gr.monaco.verificavencedor.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Player {
+    private String nome;
+    private CardHand cardHand;
+}
