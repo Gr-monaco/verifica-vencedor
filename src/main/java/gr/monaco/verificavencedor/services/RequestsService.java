acffac858c0a89313c863e0f3909808c5cbd175a
Gr-monaco <gr-monaco@hotmail.com>
2022-09-28 19:35:40 -0300

    Isolar variaveis

--- src/main/java/gr/monaco/verificavencedor/services/RequestsService.java
+++ src/main/java/gr/monaco/verificavencedor/services/RequestsService.java
@@ -7,17 +7,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Objects;
+
 @Service
 public class RequestsService {
 
     @Value("${card.api.url}")
     private String APIURL;
-
     public ResponseEntity<DeckDTO> getDeck(){
-        return new RestTemplate().getForEntity(APIURL+"new/shuffle/?deck_count=1", DeckDTO.class);
+        ResponseEntity<DeckDTO> deckResponse = new RestTemplate().getForEntity(APIURL + "new/shuffle/?deck_count=1", DeckDTO.class);
+
+        return deckResponse;
     }
 
     public ResponseEntity<CardHandDTO> getHand(String deckId, int amountOfCards){
-        return  new RestTemplate().getForEntity(APIURL+deckId+"/draw/?count="+ amountOfCards, CardHandDTO.class);
+        ResponseEntity<CardHandDTO> cardHand = new RestTemplate().getForEntity(APIURL + deckId + "/draw/?count=" + amountOfCards, CardHandDTO.class);
+        return cardHand;
     }
 }
