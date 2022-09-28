a5a9e212652ca4f011050c6387e7c2c50f300b82
Gr-monaco <gr-monaco@hotmail.com>
2022-09-27 21:42:05 -0300

    Adicionar testes para salvar deck em banco de dados

---
+++ src/test/java/gr/monaco/verificavencedor/DeckDTOSavingTest.java
@@ -0,0 +1,36 @@
+package gr.monaco.verificavencedor;
+
+import gr.monaco.verificavencedor.models.Deck;
+import gr.monaco.verificavencedor.models.DeckDTO;
+import gr.monaco.verificavencedor.models.DeckMapper;
+import gr.monaco.verificavencedor.repository.DeckRepository;
+import gr.monaco.verificavencedor.services.RequestsService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@SpringBootTest
+public class DeckDTOSavingTest {
+
+    @Autowired
+    private DeckRepository deckRepository;
+
+    @Autowired
+    private RequestsService requestsService;
+
+    @Test
+    void salvaDeckEBuscaDeck(){
+        ResponseEntity<DeckDTO> deck = requestsService.getDeck();
+
+        deckRepository.save(DeckMapper.fromDTO(Objects.requireNonNull(deck.getBody())));
+
+        Optional<Deck> deck2 = deckRepository.findById(deck.getBody().getDeckId());
+        Assertions.assertEquals(deck.getBody().getDeckId(), deck2.get().getDeckId());
+
+    }
+}
