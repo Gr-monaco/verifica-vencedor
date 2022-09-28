e92ecb609e3963aa7e013640187e5aed733bf11c
Gr-monaco <gr-monaco@hotmail.com>
2022-09-28 19:22:54 -0300

    Mudar nome de propriedade em CardHandDTO

--- src/main/java/gr/monaco/verificavencedor/models/CardHandDTO.java
+++ src/main/java/gr/monaco/verificavencedor/models/CardHandDTO.java
@@ -1,6 +1,5 @@
 package gr.monaco.verificavencedor.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +9,7 @@ import java.io.Serializable;
 @Setter //Foi adicionado getter para permitir a criação de testes
 public class CardHandDTO implements Serializable {
     private String sucess;
-    @JsonProperty("deck_id")
-    private String Id;
+    private String deckId;
     private CardDTO[] cards;
     private int remaining;
 }
--- src/main/java/gr/monaco/verificavencedor/models/CardHandMapper.java
+++ src/main/java/gr/monaco/verificavencedor/models/CardHandMapper.java
@@ -1,14 +1,11 @@
 package gr.monaco.verificavencedor.models;
 
 
-import gr.monaco.verificavencedor.repository.CardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class CardHandMapper {
 
     public static CardHand fromDTO(CardHandDTO cardHandDTO){
         CardHand cardHand = new CardHand();
-        cardHand.setDeckId(cardHandDTO.getId());
+        cardHand.setDeckId(cardHandDTO.getDeckId());
         cardHand.setCardOneId(cardHandDTO.getCards()[0].getCode());
         cardHand.setCardTwoId(cardHandDTO.getCards()[1].getCode());
         cardHand.setCardThreeId(cardHandDTO.getCards()[2].getCode());
