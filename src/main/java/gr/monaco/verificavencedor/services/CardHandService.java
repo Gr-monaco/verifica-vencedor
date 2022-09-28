b4d01cf74114f3f351b1d02d6707d29b30ada528
Gr-monaco <gr-monaco@hotmail.com>
2022-09-27 22:51:36 -0300

    Corrigir commit anterior

--- src/main/java/gr/monaco/verificavencedor/services/CardHandService.java
+++ src/main/java/gr/monaco/verificavencedor/services/CardHandService.java
@@ -1,19 +1,30 @@
 package gr.monaco.verificavencedor.services;
 
-import gr.monaco.verificavencedor.models.Card;
-import gr.monaco.verificavencedor.models.CardHand;
+import gr.monaco.verificavencedor.models.CardDTO;
+import gr.monaco.verificavencedor.models.CardHandDTO;
+import gr.monaco.verificavencedor.models.CardMapper;
+import gr.monaco.verificavencedor.repository.CardRepository;
 import gr.monaco.verificavencedor.utils.CardUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class CardHandService {
 
-    public int SumCardValues(CardHand hand){
-        List<Card> cardList = Arrays.asList(hand.getCards());
-        return cardList.stream().mapToInt(CardUtils::evaluate).sum();
+    @Autowired
+    CardRepository cardRepository;
+    public int SumCardValues(CardHandDTO hand){
+        List<CardDTO> cardDTOList = Arrays.asList(hand.getCards());
+        return cardDTOList.stream().mapToInt(CardUtils::evaluate).sum();
+    }
+
+    public void saveCardHand(CardHandDTO hand){
+        for (CardDTO card: hand.getCards()
+             ) {
+            cardRepository.save(CardMapper.fromDTO(card));
+        }
     }
 }
