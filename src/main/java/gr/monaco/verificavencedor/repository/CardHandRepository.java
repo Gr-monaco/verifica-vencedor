cdce744f46ffa21fda8246f1d4db3630f39da68f
Gr-monaco <gr-monaco@hotmail.com>
2022-09-28 18:44:29 -0300

    Implementar repositório de CardHand

---
+++ src/main/java/gr/monaco/verificavencedor/repository/CardHandRepository.java
@@ -0,0 +1,14 @@
+package gr.monaco.verificavencedor.repository;
+
+import gr.monaco.verificavencedor.models.CardHand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CardHandRepository extends JpaRepository<CardHand, Long> {
+    @Override
+    Optional<CardHand> findById(Long aLong);
+
+    @Override
+    <S extends CardHand> S save(S entity);
+}
