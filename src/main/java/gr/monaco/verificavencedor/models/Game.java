0166c96639f504915d25c4815dcc37373e053100
Gr-monaco <gr-monaco@hotmail.com>
2022-09-28 21:19:25 -0300

    Criar modelo de jogo

---
+++ src/main/java/gr/monaco/verificavencedor/models/Game.java
@@ -0,0 +1,27 @@
+package gr.monaco.verificavencedor.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long playerOneHandId;
+    private Long playerTwoHandId;
+    private Long playerThreeHandId;
+    private Long playerFourHandId;
+}
