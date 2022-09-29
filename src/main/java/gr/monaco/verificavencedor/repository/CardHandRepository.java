0d95378a725e9c39e17bc482502161594f9bf2ac
Gr-monaco <gr-monaco@hotmail.com>
2022-09-29 00:33:32 -0300

    Adicionar TODO

--- src/main/java/gr/monaco/verificavencedor/repository/CardHandRepository.java
+++ src/main/java/gr/monaco/verificavencedor/repository/CardHandRepository.java
@@ -2,6 +2,7 @@ package gr.monaco.verificavencedor.repository;
 
 import gr.monaco.verificavencedor.models.CardHand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ public interface CardHandRepository extends JpaRepository<CardHand, Long> {
     @Override
     <S extends CardHand> S save(S entity);
 
+    //TODO: Colocar em Query
     Optional<CardHand> findByDeckIdAndCardOneIdAndCardTwoIdAndCardThreeIdAndCardFourIdAndCardFiveId(String deckId, String cardOneId, String cardTwoId,
                                                                                      String cardThreeId, String cardFourId,
                                                                                      String cardFiveId);
